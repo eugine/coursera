dd8ba3461d9119bf50267d845475bb35a4678da4
Eugene Sokolov <eugine_s@ukr.net>
2013-02-15 17:48:52 +0200

    Fixed readme and Taks1 description

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ coursera
 ========
 
 Task done for courses: http://www.coursera.org
+
+https://www.coursera.org/course/algs4partI
+
--- src/org/coursera/algs4/tasks/week1/unionfind/Task1.java
+++ src/org/coursera/algs4/tasks/week1/unionfind/Task1.java
@@ -2,10 +2,21 @@ package org.coursera.algs4.tasks.week1.unionfind;
 
 import org.coursera.algs4.QuickFindAlgorithm;
 
+/**
+ * Question 1 (seed = 481358)<br> 
+ * Give the id[] array that results from the following sequence of 6 union operations on a set of 10 items using the quick-find algorithm.<br><br> 
+ * 
+ * 9-8 5-7 0-2 3-9 1-8 5-0 <br><br>
+ * 
+ * Recall: our quick-find convention for the union operation p-q is to change id[p] (and perhaps some other entries) but not id[q]. 
+ * 
+ * @author e.sokolov
+ *
+ */
 public class Task1 {
 
 	/**
-	 * @param args
+	 * @param args - ignored
 	 */
 	public static void main(String[] args) {
 		QuickFindAlgorithm qf = new QuickFindAlgorithm(10);
