b3420f4e1e637344abfc606f8084dc59675f458e
Eugene Sokolov <eugine_s@ukr.net>
2013-03-12 19:01:50 +0200

    Added RandomizedQueue.

---
+++ src/RandomizedQueue.java
@@ -0,0 +1,68 @@
+import java.util.Iterator;
+
+public class RandomizedQueue<Item> {
+
+	
+	/**
+	 * construct an empty randomized queue
+	 */
+	public RandomizedQueue() {
+
+	}
+
+	/**
+	 * is the queue empty?
+	 * 
+	 * @return
+	 */
+	public boolean isEmpty() {
+		return true;
+
+	}
+
+	/**
+	 * return the number of items on the queue
+	 * 
+	 * @return
+	 */
+	public int size() {
+		return 0;
+	}
+
+	/**
+	 * add the item
+	 * 
+	 * @param item
+	 */
+	public void enqueue(Item item) {
+
+	}
+
+	/**
+	 * delete and return a random item
+	 * 
+	 * @return
+	 */
+	public Item dequeue() {
+		return null;
+	}
+
+	/**
+	 * return (but do not delete) a random item
+	 * 
+	 * @return
+	 */
+	public Item sample() {
+		return null;
+	}
+
+	/**
+	 * return an independent iterator over items in random order
+	 * 
+	 * @return
+	 */
+	public Iterator<Item> iterator() {
+		return null;
+	}
+
+}
